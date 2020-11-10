595edfa86116b63d8a153f30add7be9900561977
leetlejohn <45561466+leetlejohn@users.noreply.github.com>
2020-11-10 21:06:33 +0000

    Task 3.4.5 fixed

---
+++ scripts/task-3.4.5.js
@@ -0,0 +1,17 @@
+var temperature = -25;
+
+
+if (temperature < 0) {
+    console.log('Just stay inside'); 
+
+ } else if (temperature < 30) {
+ console.log('Put on your coat and hat');
+
+} else if (temperature < 50) {
+    console.log('Put on your coat'); }
+
+else {
+    console.log('Just pants and vest is fine');
+}
+
+//this logic is not working
