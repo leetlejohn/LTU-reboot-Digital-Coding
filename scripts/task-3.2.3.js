16ffcad7a898605b280ec7a8038027cc0e207901
leetlejohn <45561466+leetlejohn@users.noreply.github.com>
2020-11-10 20:19:58 +0000

    Added comments to code

--- scripts/task-3.2.3.js
+++ scripts/task-3.2.3.js
@@ -5,8 +5,10 @@ function addName(firstName, lastName){
     return(firstName, lastName);
 }
 
+// populate variable using function
+
 var userName = addName('Curtis' , 'Schmurtis'); 
 
-
+//output variable
 
 console.log(userName); //not sure why this only conole logs the last name
