5655ae69951e7dcbc6ed553bd3fe86fc12023ce4
leetlejohn <45561466+leetlejohn@users.noreply.github.com>
2020-11-12 19:11:09 +0000

    Update task-3.2.5.js
    Formatting updates to neaten code

--- scripts/task-3.2.5.js
+++ scripts/task-3.2.5.js
@@ -2,16 +2,15 @@ var temperature = -25;
 
 
 if (temperature < 0) {
-    console.log('Just stay inside'); 
-
- } else if (temperature < 30) {
- console.log('Put on your coat and hat');
-
-} else if (temperature < 50) {
+    console.log('Just stay inside'); } 
+    
+else if (temperature < 30) {
+ console.log('Put on your coat and hat'); } 
+ 
+else if (temperature < 50) {
     console.log('Put on your coat'); }
 
 else {
-    console.log('Just pants and vest is fine');
-}
+    console.log('Just pants and vest is fine'); }
 
 //this logic wasn't working but I think it was down to the order
