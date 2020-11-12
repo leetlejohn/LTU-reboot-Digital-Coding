7576e15da35435c6c6128c1957d8e03dcf66ca90
leetlejohn <45561466+leetlejohn@users.noreply.github.com>
2020-11-12 19:27:42 +0000

    Update task-3.2.6.js

---
+++ scripts/task-3.2.6.js
@@ -0,0 +1,25 @@
+var temperature = -25;
+
+
+if (temperature < 0) {
+    console.log('Just stay inside'); } 
+    
+else if (temperature < 30) {
+    console.log('Put on your coat and hat'); } 
+ 
+else if (temperature < 50) {
+    console.log('Put on your coat'); }
+
+
+else if (temperature > 50 && temperature === 55)  {
+    console.log('It is very hot'); }
+
+else if (temperature > 50 || temperature === 55)  {
+
+    console.log('It is extremely hot, looks like someone is going to get burned'); }
+
+else {
+    console.log('Just pants and vest is fine'); }
+
+// logical operators:    && (AND)    || (OR)    ! (NOT)
+
