40ed90b133a8a34cc806170d751cde5f88ce9680
leetlejohn <45561466+leetlejohn@users.noreply.github.com>
2020-11-17 20:27:13 +0000

    Update task-3.4.3.js

---
+++ scripts/task-3.4.3.js
@@ -0,0 +1,9 @@
+var favouriteFoods = ['Pizza', 'Pasta', 'Jalrezi', 'Carbonara', 'Chips'];
+
+//for(var index = 0; index < favouriteFoods.length; index++ ) {
+
+    document.write( favouriteFoods[2] + '<br>');
+    document.write( favouriteFoods[4] + '<br>');
+    document.write( favouriteFoods[3] + '<br>');
+
+//}
