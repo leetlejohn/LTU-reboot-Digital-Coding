2a4f4339a2bc8ddcf000a22514ea7a165bf5bf39
leetlejohn <45561466+leetlejohn@users.noreply.github.com>
2020-11-17 20:21:23 +0000

    Task 3.4.2 content and 3.4.3 setup

---
+++ scripts/task-3.4.2.js
@@ -0,0 +1,6 @@
+var favouriteFoods = ['Pizza', 'Pasta', 'Jalrezi', 'Carbonara', 'Chips'];
+
+for(var index = 0; index < favouriteFoods.length; index++ ) {
+
+    console.log( favouriteFoods[index]);
+}
