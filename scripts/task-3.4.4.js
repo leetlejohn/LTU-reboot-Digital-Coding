7fcd4204ca0b4c8d1347c3a9363cc05e857ec72a
leetlejohn <45561466+leetlejohn@users.noreply.github.com>
2020-11-19 19:21:45 +0000

    Update task-3.4.4.js
    Listing ingredients with a loop

--- scripts/task-3.4.4.js
+++ scripts/task-3.4.4.js
@@ -12,4 +12,11 @@ directions: ["Cook the food", "Cook the food more"],
 
 document.write("I'm hungry let's cook " + recipe.recipeTitle );
 
+var ingredients = recipe.ingredients;
+
+for( var index = 0; index < ingredients.length; index++ ) {
+
+    document.write('<br>' + ingredients[index]);
+}
+
 // this doesn't work as a function but I'll work out why later
