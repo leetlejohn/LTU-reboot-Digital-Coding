72f381ada6a0627b6622ae796b82f1d34f652243
leetlejohn <45561466+leetlejohn@users.noreply.github.com>
2020-11-19 19:17:30 +0000

    Update task-3.4.4.js
    From console log to document.write

--- scripts/task-3.4.4.js
+++ scripts/task-3.4.4.js
@@ -10,6 +10,6 @@ directions: ["Cook the food", "Cook the food more"],
 
 };
 
-console.log("I'm hungry let's cook " + recipe.recipeTitle );
+document.write("I'm hungry let's cook " + recipe.recipeTitle );
 
 // this doesn't work as a function but I'll work out why later
