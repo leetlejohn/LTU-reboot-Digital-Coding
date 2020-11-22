bfa7f386e2740fe90ba9e5fa5655ab1b398af8e4
leetlejohn <45561466+leetlejohn@users.noreply.github.com>
2020-11-22 18:31:09 +0000

    Update task-3.5.3.js

---
+++ scripts/task-3.5.3.js
@@ -0,0 +1,101 @@
+//Define an array of objects which will make up the shopping cart
+
+let shoppingCart = [
+    {
+    name:"loaf of bread",
+    type:"food",
+    quantity:1,
+    price:.85
+    },
+    {
+    name:"multipack beans",
+    type:"food",
+    quantity:1,
+    price:1
+    },
+    {
+    name:"mushrooms",
+    type:"food",
+    quantity:10,
+    price:.1
+    },
+    {
+    name:"can of beer",
+    type:"alcohol",
+    quantity:4,
+    price:1.1
+    },
+    {
+    name:"prosecco",
+    type:"alcohol",
+    quantity:1,
+    price:8.99
+    },
+    {
+    name:"steak",
+    type:"food",
+    quantity:2,
+    price:3.99
+    },
+    {
+    name:"blue cheese",
+    type:"food",
+    quantity:1,
+    price:2.99
+    },
+    {
+    name:"candles",
+    type:"home",
+    quantity:3,
+    price:1.99
+    },
+    {
+    name:"cheesecake",
+    type:"food",
+    quantity:1,
+    price:4.99
+    },
+    {
+    name:"onions",
+    type:"food",
+    quantity:3,
+    price:.4
+    }];
+
+
+
+    // function to find discount amount
+
+    function discountAmount ( arr, discountAmount, type) {  //1. Add 2 extra arguments...
+
+        var totalPrice = 0;
+
+        for ( var index=0; index < arr.length; index++ )  {
+
+            if ( arr[index].type === type )  {   //2. replace logic....
+
+                var discount = ( arr[index].price * discountAmount) / 100; 
+
+                totalPrice = totalPrice + ( arr[index].price - discount) * arr[index].quantity;
+
+            } else if ( type === 'any' ) {
+
+                var discount = ( arr[index].price * discountAmount) / 100; 
+
+                totalPrice = totalPrice + ( arr[index].price - discount) * arr[index].quantity;
+
+            } else {
+
+
+                totalPrice = totalPrice + ( arr[index].price * arr[index].quantity);
+            }
+
+
+        }
+
+
+        return totalPrice.toFixed(2);
+
+    }
+
+  console.log(discountAmount( shoppingCart, 90, 'alcohol') );
