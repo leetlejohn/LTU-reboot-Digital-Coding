a4c4173607e49e29074828c77c0ac69370f88e10
leetlejohn <45561466+leetlejohn@users.noreply.github.com>
2020-11-22 16:23:40 +0000

    Update task-3.4.4.js

--- scripts/task-3.4.4.js
+++ scripts/task-3.4.4.js
@@ -1,8 +1,8 @@
 var recipe = {
 recipeTitle: 'Spaghetti Carbonara',
 servings: 3,
-ingredients: ["nutmeg", "parmesan", "spaghetti", "pork goujons"],
-directions: ["Cook the food", "Cook the food more"],
+ingredients: ['nutmeg', 'parmesan', 'spaghetti', 'pork goujons'],
+directions: ['Cook the food', 'Cook the food more'],
 letsCook: function() {
 
     document.write("I'm hungry let's cook " + recipe.recipeTitle );
@@ -22,3 +22,7 @@ for( var index = 0; index < ingredients.length; index++ ) {
     document.write( '<br> <li>' + ingredients[index]);
 }
 
+//examples of Object.keys and entries
+
+document.write( '<br> <br>' + Object.keys(recipe) );
+document.write( '<br> <br>' + Object.entries(recipe) );
