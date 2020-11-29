a9aabab45152686ea1aad5f4bf169849dfd6b0dd
leetlejohn <45561466+leetlejohn@users.noreply.github.com>
2020-11-29 12:58:13 +0000

    Create and update task 3.6.1
    Hello there

---
+++ scripts/task-3.6.1.js
@@ -0,0 +1,12 @@
+var paragraph = document.querySelectorAll('p');
+
+paragraph.forEach( function(para) {
+
+para.style.color = "green";
+} );
+
+
+var image = document.querySelector('img');
+
+image.src = "https://i.pinimg.com/736x/01/49/70/0149705f8e38df8846bb070cbc475f96.jpg";
+image.style.width = '300px';
