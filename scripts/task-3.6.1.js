a0daf3506dcbe576ca64abf0e2ec5f44aba4e697
leetlejohn <45561466+leetlejohn@users.noreply.github.com>
2020-11-29 13:00:34 +0000

    Update task-3.6.1.js
    improving selector

--- scripts/task-3.6.1.js
+++ scripts/task-3.6.1.js
@@ -8,5 +8,5 @@ para.style.color = "green";
 
 var image = document.querySelector('img');
 
-image.src = "https://i.pinimg.com/736x/01/49/70/0149705f8e38df8846bb070cbc475f96.jpg";
+image.setAttribute ('src', 'https://i.pinimg.com/736x/01/49/70/0149705f8e38df8846bb070cbc475f96.jpg');
 image.style.width = '300px';
